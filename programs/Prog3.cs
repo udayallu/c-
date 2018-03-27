9ca996e311c400a26fe1f3d61dc02ebfdfdb6ce3
UDAY ALLU <udayallu2010@gmail.com>
2018-03-27 14:36:37 +0530

    Add files via upload

---
+++ programs/Prog3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Message");
+            Console.WriteLine("you said " + "uday");
+        }
+    }
+}
---
+++ programs/Prog4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace program4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the name of your band");
+            string name=Console.ReadLine();
+            Console.WriteLine(" How many people are in your band");
+            int num=int.Parse(Console.ReadLine());
+            Console.WriteLine(name + "has" + num);
+
+        }
+    }
+}
