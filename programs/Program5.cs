cbc0263e38dbea2a3c6d5c6575d0772520de3caf
UDAY ALLU <udayallu2010@gmail.com>
2018-03-27 20:09:53 +0530

    Add files via upload

---
+++ programs/Program5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace program4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the name of your band");
+            string name=Console.ReadLine();
+            Console.WriteLine("How many people are in your band?");
+             int numberOfMembers = 0;
+           if(int.TryParse(Console.ReadLine(), out numberOfMembers))
+           {
+             Console.WriteLine(name + " band has " + numberOfMembers); 
+              Environment.Exit(0);
+           }
+           else
+           {
+             Console.WriteLine("input was not valid"); 
+             Environment.Exit(0);
+           }
+
+        }
+    }
+}
